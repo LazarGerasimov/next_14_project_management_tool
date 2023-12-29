41d6a493d40197f4d6910e572e9e6e2dda750f55
Laz <lazgerasimov@gmail.com>
2023-12-29 19:11:12 +0200

    create marketing page scaffold

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+        Marketing Page
+    </div>
+  )
+}
+
+export default MarketingPage;
