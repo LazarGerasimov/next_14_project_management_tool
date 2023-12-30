695b192b28726be0a88bfce4451fdcb23e4ace65
Laz <lazgerasimov@gmail.com>
2023-12-30 08:09:37 +0200

    update marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,23 @@
+import { Medal } from "lucide-react";
 
 const MarketingPage = () => {
   return (
     <div className="flex items-center justify-center flex-col">
-        Marketing Page
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+          <Medal className="h-6 w-6 mr-2" />
+          The reliable task manager
+        </div>
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+          Taskerr helps you move
+        </h1>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-500 to-pink-200 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          work forward
+        </div>
+      </div>
+      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+        Collaborate, manage projects and reach new productivity peaks. From corporate environment to the home office, create a unique way of working with Taskerr.
+      </div>
     </div>
   )
 }
