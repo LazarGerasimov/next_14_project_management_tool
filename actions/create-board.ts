e4a038e660a1010e1256b6935a9bcc81adf86469
Laz <lazgerasimov@gmail.com>
2024-01-09 12:01:28 +0200

    create safe-action handler

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -4,19 +4,45 @@ import { z } from 'zod';
 
 import { db } from "@/lib/db";
 
+export type State = {
+  errors?: {
+    title?: string[];
+  },
+  message?: string | null;
+}
+
 const CreateBoardValidation = z.object({
-  title: z.string(),
+  title: z.string().min(3, {
+    message: "Minimum length of 3 letters is required"
+  }),
 });
 
-export async function createBoard(formData: FormData) {
+export async function createBoard(prevState: State, formData: FormData) {
 
-  const { title } = CreateBoardValidation.parse({
+  const validatedFields = CreateBoardValidation.safeParse({
     title: formData.get("title")
   })
 
-  await db.board.create({
-    data: {
-      title,
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields."
     }
-  })
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      }
+    })
+  } catch (error) {
+    return {
+      message: "Database Error"
+    }
+  }
+
+  
 }
