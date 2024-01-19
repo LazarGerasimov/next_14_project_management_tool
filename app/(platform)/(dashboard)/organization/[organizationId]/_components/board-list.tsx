ae7a1ee308a888eb374d6373c3bf34851645c448
Laz <lazgerasimov@gmail.com>
2024-01-19 10:13:08 +0200

    add board list skeleton fallback

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,5 +1,6 @@
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/ui/hint";
+import { Skeleton } from "@/components/ui/skeleton";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2Icon } from "lucide-react";
@@ -64,3 +65,20 @@ const BoardList = async () => {
 };
 
 export default BoardList;
+
+BoardList.Skeleton = function SkeletonBoardList() {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+      <Skeleton className="aspect-video h-full w-full p-2" />
+    </div>
+  )
+};
+
