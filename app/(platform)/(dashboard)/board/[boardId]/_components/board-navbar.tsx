845f822f783668c58904c373f0f0e81df15809e8
Laz <lazgerasimov@gmail.com>
2024-01-22 20:17:20 +0200

    add board title form scaffold

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,6 @@
 
 import { Board } from "@prisma/client";
+import BoardTitleForm from "./board-title-form";
 
 interface BoardNavbarProps {
   data: Board
@@ -10,8 +11,8 @@ const BoardNavbar = async ({
 }: BoardNavbarProps) => {
 
   return (
-    <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
-      Board Navbar
+    <div className="w-full h-14 z-[40] bg-black/30 fixed top-14 flex items-center px-6 gap-x-4 text-white">
+      <BoardTitleForm data={data} />
     </div>
   )
 }
