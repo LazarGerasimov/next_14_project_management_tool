65ffb59e93981baf6d21e887bc86152ca6c10b78
Laz <lazgerasimov@gmail.com>
2024-01-22 19:59:12 +0200

    add dynamic board title change functionality

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -2,42 +2,66 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { notFound, redirect } from "next/navigation";
 
+export async function generateMetadata({
+  params
+}: {
+  params: { boardId: string }
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    }
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId
+    }
+  });
+
+  return {
+    title: board?.title || "Board"
+  }
+}
 
 const BoardIdLayout = async ({
-    children,
-    params,
+  children,
+  params,
 }: {
-    children: React.ReactNode;
-    params: { boardId: string }
+  children: React.ReactNode;
+  params: { boardId: string }
 }) => {
 
-    const { orgId } = auth();
+  const { orgId } = auth();
 
-    if (!orgId) {
-        redirect("/select-org")
+  if (!orgId) {
+    redirect("/select-org")
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId: orgId
     }
+  });
 
-    const board = await db.board.findUnique({
-        where: {
-            id: params.boardId,
-            orgId: orgId
-        }
-    });
+  if (!board) {
+    notFound();
+  }
 
-    if (!board) {
-        notFound();
-    }
-
-    return (
-        <div
-            className="relative h-full bg-no-repeat bg-cover bg-center"
-            style={{ backgroundImage: `url(${board.imageFullUrl})` }}
-        >
-            <main className="relative pt-28 h-full">
-                {children}
-            </main>
-        </div>
-    )
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">
+        {children}
+      </main>
+    </div>
+  )
 };
 
 export default BoardIdLayout;
