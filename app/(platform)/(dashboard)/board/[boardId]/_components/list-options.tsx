4f32593f42d85bfa415cfc8ebb73af48fba2bb8c
Laz <lazgerasimov@gmail.com>
2024-01-27 18:44:41 +0200

    implement delete list functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -11,6 +11,9 @@ import { Button } from "@/components/ui/button";
 import { MoreHorizontal, X } from "lucide-react";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
+import { toast } from "sonner";
 
 
 interface ListOptionsProps {
@@ -22,6 +25,23 @@ const ListOptions = ({
   data,
   onAddCard
 }: ListOptionsProps) => {
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success("List deleted successfully.");
+    },
+    onError: (error) => {
+      toast.error(error)
+    }
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({ id, boardId });
+  }
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -56,7 +76,7 @@ const ListOptions = ({
           </FormSubmit>
         </form>
         <Separator />
-        <form action="">
+        <form action={onDelete}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
