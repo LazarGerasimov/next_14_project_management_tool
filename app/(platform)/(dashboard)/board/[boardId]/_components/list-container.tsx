17d35f596187d44886669f2bbe793e0b35e1108a
Laz <lazgerasimov@gmail.com>
2024-01-27 14:10:35 +0200

    update lists ui

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,24 +2,44 @@
 
 import { ListWithCards } from "@/types";
 import ListForm from "./list-form";
+import { useEffect, useState } from "react";
+import ListItem from "./list-item";
 
 interface ListContainerProps {
-    data: ListWithCards[];
-    boardId: string;
+	data: ListWithCards[];
+	boardId: string;
 }
 
 
 
 const ListContainer = ({
-    data,
-    boardId
+	data,
+	boardId
 }: ListContainerProps) => {
-    return (
-        <ol>
-            <ListForm />
-            <div className="flex-shrink 0 w-1" />
-        </ol>
-    )
+
+	const [orderedData, setOrderedData] = useState(data);
+
+	// Optimistic update
+
+	useEffect(() => {
+		setOrderedData(data);
+	}, [data]);
+
+	return (
+		<ol className="flex gap-x-3 h-full">
+			{orderedData.map((list, index) => {
+				return (
+					<ListItem
+						key={list.id}
+						index={index}
+						data={list}
+					/>
+				)
+			})}
+			<ListForm />
+			<div className="flex-shrink 0 w-1" />
+		</ol>
+	)
 }
 
 export default ListContainer
