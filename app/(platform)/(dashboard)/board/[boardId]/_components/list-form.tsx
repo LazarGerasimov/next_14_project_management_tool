0e55bff38757453792d1830b7a46141a49e21b87
Laz <lazgerasimov@gmail.com>
2024-01-27 13:50:43 +0200

    implement create list functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -5,14 +5,17 @@ import { useState, useRef, ElementRef } from "react";
 import ListWrapper from "./list-wrapper";
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { FormInput } from "@/components/form/form-input";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { FormSubmit } from "@/components/form/form-submit";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
+import { toast } from "sonner";
 
 
 const ListForm = () => {
-
   const params = useParams();
+  const router = useRouter();
 
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
@@ -30,6 +33,17 @@ const ListForm = () => {
     setIsEditing(false);
   };
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} created successfully!`);
+      disableEditing();
+      router.refresh();
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  })
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Esc") {
       disableEditing();
@@ -39,12 +53,20 @@ const ListForm = () => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+
+    execute({ title, boardId });
+  }
+
   if (isEditing) {
     return (
       <ListWrapper>
-        <form ref={formRef} className="w-full p-3 rounded-md bg-white space-y-4 shadow-md">
+        <form action={onSubmit} ref={formRef} className="w-full p-3 rounded-md bg-white space-y-4 shadow-md">
           <FormInput
             ref={inputRef}
+            errors={fieldErrors}
             id="title"
             className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
             placeholder="Enter list title.."
