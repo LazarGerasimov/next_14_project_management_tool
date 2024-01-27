2fae3b78ab50e702b4a13f0d162ec3d0d6b49377
Laz <lazgerasimov@gmail.com>
2024-01-27 18:56:36 +0200

    add copy-list server action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -14,6 +14,7 @@ import { Separator } from "@/components/ui/separator";
 import { useAction } from "@/hooks/use-action";
 import { deleteList } from "@/actions/delete-list";
 import { toast } from "sonner";
+import { ElementRef, useRef } from "react";
 
 
 interface ListOptionsProps {
@@ -26,9 +27,12 @@ const ListOptions = ({
   onAddCard
 }: ListOptionsProps) => {
 
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const { execute: executeDelete } = useAction(deleteList, {
     onSuccess: (data) => {
       toast.success(`List "${data.title}" deleted successfully.`);
+      closeRef?.current?.click();
     },
     onError: (error) => {
       toast.error(error)
@@ -53,7 +57,7 @@ const ListOptions = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           List actions
         </div>
-        <PopoverClose asChild>
+        <PopoverClose asChild ref={closeRef}>
           <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant={"ghost"}>
             <X className="h-4 w-4" />
           </Button>
