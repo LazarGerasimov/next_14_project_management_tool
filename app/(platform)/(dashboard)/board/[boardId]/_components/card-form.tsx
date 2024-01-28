b7ce6dc910b027df9ef7cad209b9b02688c58b60
Laz <lazgerasimov@gmail.com>
2024-01-28 21:11:46 +0200

    add card textarea scaffold

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { FormSubmit } from "@/components/form/form-submit";
+import FormTextArea from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 
@@ -18,6 +20,31 @@ const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
   disableEditing,
   isEditing
 }: CardFormProps, ref) => {
+
+  if (isEditing) {
+    return (
+      <form action=""
+        className="m-1 py-0.5 px-1 space-y-4"
+      >
+        <FormTextArea
+          id="title"
+          onKeyDown={() => { }}
+          ref={ref}
+          placeholder="Enter a title for this card..."
+        />
+        <input hidden id="listId" name="listId" value={listId} />
+        <div className="flex items-center gap-x-1">
+          <FormSubmit>
+            Add card
+          </FormSubmit>
+          <Button onClick={disableEditing} size={"sm"} variant={"ghost"}>
+            <X className="h-5 w-5" />
+          </Button>
+        </div>
+      </form>
+    )
+  }
+
   return (
     <div className="pt-2 px-2">
       <Button
