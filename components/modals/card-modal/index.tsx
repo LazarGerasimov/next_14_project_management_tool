578d9ec4a5db0665be61abcf6f3a49954ea2ec7b
Laz <lazgerasimov@gmail.com>
2024-02-01 19:03:00 +0200

    upd card modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { useQuery } from '@tanstack/react-query'
+
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { CardWithList } from '@/types';
+import { fetcher } from '@/lib/fetcher';
 
 export const CardModal = () => {
 
@@ -9,13 +13,21 @@ export const CardModal = () => {
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`)
+  });
+
+
+
+
   return (
     <Dialog
       open={isOpen}
       onOpenChange={onClose}
     >
       <DialogContent>
-        I am a modal
+        {cardData?.title}
       </DialogContent>
     </Dialog>
   );
