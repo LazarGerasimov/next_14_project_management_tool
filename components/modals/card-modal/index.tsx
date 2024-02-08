94a8c873b69727c4601f3bc56eff28700bd9d3d2
Laz <lazgerasimov@gmail.com>
2024-02-08 15:03:40 +0200

    add extracted logs to card modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -9,6 +9,7 @@ import { fetcher } from '@/lib/fetcher';
 import { Header } from './header';
 import Description from './description';
 import Actions from './actions';
+import { AuditLog } from '@prisma/client';
 
 export const CardModal = () => {
 
@@ -19,6 +20,11 @@ export const CardModal = () => {
   const { data: cardData } = useQuery<CardWithList>({
     queryKey: ["card", id],
     queryFn: () => fetcher(`/api/cards/${id}`)
+  });
+
+  const { data: auditLogsData } = useQuery<AuditLog[]>({
+    queryKey: ["card-logs", id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`)
   });
 
 
