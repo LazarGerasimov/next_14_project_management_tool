f5bc25386f5653cbd1201b746599e7c1004bd8a6
Laz <lazgerasimov@gmail.com>
2024-02-08 13:52:27 +0200

    create card action buttons scaffolds

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/types";
+import { Copy, Trash } from "lucide-react";
 
 interface ActionsProps {
   data: CardWithList;
@@ -11,8 +13,24 @@ const Actions = ({
   data
 }: ActionsProps) => {
   return (
-    <div>
-      <p>Actions</p>
+    <div className="space-y-2 mt-2">
+      <p className="text-xs font-semibold">Actions</p>
+      <Button
+        variant={"gray"}
+        className="w-full justify-start"
+        size={"inline"}
+      >
+        <Copy className="h-4 w-4 mr-2" />
+        Copy
+      </Button>
+      <Button
+        variant={"gray"}
+        className="w-full justify-start"
+        size={"inline"}
+      >
+        <Trash className="h-4 w-4 mr-2" />
+        Delete
+      </Button>
     </div>
   )
 };
