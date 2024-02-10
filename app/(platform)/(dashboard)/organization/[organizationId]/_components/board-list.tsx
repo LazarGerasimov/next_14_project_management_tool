e4ce6b6d02a42b43ed86bb0fd7b0c47f87184d2e
Laz <lazgerasimov@gmail.com>
2024-02-10 17:59:14 +0200

    implement incremented count on board creation

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,11 +1,14 @@
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/ui/hint";
 import { Skeleton } from "@/components/ui/skeleton";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
 import { db } from "@/lib/db";
+import { getAvailableCount } from "@/lib/org-limit";
 import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2Icon } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+
 
 const BoardList = async () => {
 
@@ -23,6 +26,9 @@ const BoardList = async () => {
       createdAt: 'desc'
     }
   });
+
+  const availableCount = await getAvailableCount();
+
 
   return (
     <div className="space-y-4">
@@ -50,7 +56,7 @@ const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${MAX_FREE_BOARDS - availableCount} remaning`}</span>
             <Hint sideOffset={40} description={`
             Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.
             `}>
